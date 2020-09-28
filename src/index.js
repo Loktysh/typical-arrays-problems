7120e63fb9f6794a37c0851722b0f471470c77ec
Loktysh Alexandr <vebster24@mail.ru>
2020-09-28 22:50:58 +0300

    feat: add max function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  
+  for (var i = 0, el = 0; i < array.length; i++) {
+    if (array[i] > el && array.length !== undefined) {
+      el = array[i];
+    } else {
+      
+    }
+  }
+  return el;
 }
 
 exports.avg = function avg (array) {
