4e5368bc76b0d77582082fd6954ad5f5a2663c72
Loktysh Alexandr <vebster24@mail.ru>
2020-09-28 22:51:20 +0300

    feat: add max function

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ exports.max = function max (array) {
   if (array === undefined) {
     return 0;
   }
-  
   for (var i = 0, el = 0; i < array.length; i++) {
     if (array[i] > el && array.length !== undefined) {
       el = array[i];
